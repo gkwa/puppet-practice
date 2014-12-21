b5f3830eb8f2343549a598bea350c913a5df4894
taylormonacelli <taylormonacelli@gmail.com>
2014-12-21 11:05:21 -0800

    yada-yada

--- t1/manifests/site.pp
+++ t1/manifests/site.pp
@@ -18,12 +18,3 @@ node 'sbx529eff0','t1','t2','t3' {
     source_permissions => ignore,
   }
 }
-
-node 't1' {
-  file {'c:/Windows/Temp/sensu-0.14.0-1.msi':
-    ensure             => present,
-    path               => 'c:/Windows/Temp/sensu-0.14.0-1.msi',
-    source             => 'puppet:///files/sensu-0.14.0-1.msi',
-    source_permissions => ignore,
-  }
-}
