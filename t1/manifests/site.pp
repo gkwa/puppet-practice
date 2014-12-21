1196eb7f35345078379d56622ab11d2de6c212f2
taylormonacelli <taylormonacelli@gmail.com>
2014-12-20 20:11:26 -0800

    Properly installs Sensu MSI on client

--- t1/manifests/site.pp
+++ t1/manifests/site.pp
@@ -1,8 +1,25 @@
+node 'sbx529eff0' {
+
+  package { 'Sensu':
+    ensure          => 'installed',
+    source          => 'c:/Windows/Temp/sensu-0.14.0-1.msi',
+    install_options => ['INSTALLDIR=C:\opt'],
+    require => File['c:/Windows/Temp/sensu-0.14.0-1.msi'],
+  }
+
+  file {'c:/Windows/Temp/sensu-0.14.0-1.msi':
+    ensure             => present,
+    path               => 'c:/Windows/Temp/sensu-0.14.0-1.msi',
+    source             => 'puppet:///files/sensu-0.14.0-1.msi',
+    source_permissions => ignore,
+  }
+}
+
 node 't1' {
   file {'c:/Windows/Temp/sensu-0.14.0-1.msi':
-    ensure  => present,
-    path    => 'c:/Windows/Temp/sensu-0.14.0-1.msi',
-    source  => 'puppet:///files/sensu-0.14.0-1.msi',
+    ensure             => present,
+    path               => 'c:/Windows/Temp/sensu-0.14.0-1.msi',
+    source             => 'puppet:///files/sensu-0.14.0-1.msi',
     source_permissions => ignore,
   }
 }
